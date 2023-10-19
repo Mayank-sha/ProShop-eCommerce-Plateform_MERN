e81f5d6799ad8163ae0edd0626d8fef32840b6b2
Mayank-sha <officialmayankks@gmail.com>
2023-10-19 09:19:12 +0530

    placeorder screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,8 +4,7 @@ import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [] };
-//   : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -32,18 +31,21 @@ const cartSlice = createSlice({
       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
       return updateCart(state);
     },
-    // saveShippingAddress: (state, action) => {
-    //   state.shippingAddress = action.payload;
-    //   localStorage.setItem("cart", JSON.stringify(state));
-    // },
-    // savePaymentMethod: (state, action) => {
-    //   state.paymentMethod = action.payload;
-    //   localStorage.setItem("cart", JSON.stringify(state));
-    // },
-    // clearCartItems: (state, action) => {
-    //   state.cartItems = [];
-    //   localStorage.setItem("cart", JSON.stringify(state));
-    // },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload;
+      return updateCart(state);
+      // localStorage.setItem("cart", JSON.stringify(state));
+    },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
+      return updateCart(state);
+      // localStorage.setItem("cart", JSON.stringify(state));
+    },
+    clearCartItems: (state, action) => {
+      state.cartItems = [];
+      return updateCart(state);
+      // localStorage.setItem("cart", JSON.stringify(state));
+    },
     // // NOTE: here we need to reset state for when a user logs out so the next
     // // user doesn't inherit the previous users cart and shipping
     // resetCart: (state) => (state = initialState),
@@ -53,9 +55,9 @@ const cartSlice = createSlice({
 export const {
   addToCart,
   removeFromCart,
-  //   saveShippingAddress,
-  //   savePaymentMethod,
-  //   clearCartItems,
+  saveShippingAddress,
+  savePaymentMethod,
+  clearCartItems,
   //   resetCart,
 } = cartSlice.actions;
 
