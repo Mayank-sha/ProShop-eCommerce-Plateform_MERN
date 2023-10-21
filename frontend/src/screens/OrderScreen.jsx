891091a8aacfcdeecfd7de7ae403edb805b98680
Mayank-sha <officialmayankks@gmail.com>
2023-10-21 08:46:33 +0530

    orderScreen

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,16 +1,16 @@
-// import { useEffect } from 'react';
+import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
-// import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
-// import { useSelector } from 'react-redux';
-// import { toast } from 'react-toastify';
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import {
   //   useDeliverOrderMutation,
   useGetOrderDetailsQuery,
-  //   useGetPaypalClientIdQuery,
-  //   usePayOrderMutation,
+  useGetPaypalClientIdQuery,
+  usePayOrderMutation,
 } from "../slices/ordersApiSlice";
 
 const OrderScreen = () => {
@@ -23,78 +23,78 @@ const OrderScreen = () => {
     error,
   } = useGetOrderDetailsQuery(orderId);
 
-  //  const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
+  const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
 
   //  const [deliverOrder, { isLoading: loadingDeliver }] =
   //    useDeliverOrderMutation();
 
-  //  const { userInfo } = useSelector((state) => state.auth);
+  const { userInfo } = useSelector((state) => state.auth);
 
-  //  const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
+  const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
 
-  //  const {
-  //    data: paypal,
-  //    isLoading: loadingPayPal,
-  //    error: errorPayPal,
-  //  } = useGetPaypalClientIdQuery();
+  const {
+    data: paypal,
+    isLoading: loadingPayPal,
+    error: errorPayPal,
+  } = useGetPaypalClientIdQuery();
 
-  //  useEffect(() => {
-  //    if (!errorPayPal && !loadingPayPal && paypal.clientId) {
-  //      const loadPaypalScript = async () => {
-  //        paypalDispatch({
-  //          type: 'resetOptions',
-  //          value: {
-  //            'client-id': paypal.clientId,
-  //            currency: 'USD',
-  //          },
-  //        });
-  //        paypalDispatch({ type: 'setLoadingStatus', value: 'pending' });
-  //      };
-  //      if (order && !order.isPaid) {
-  //        if (!window.paypal) {
-  //          loadPaypalScript();
-  //        }
-  //      }
-  //    }
-  //  }, [errorPayPal, loadingPayPal, order, paypal, paypalDispatch]);
+  useEffect(() => {
+    if (!errorPayPal && !loadingPayPal && paypal.clientId) {
+      const loadPaypalScript = async () => {
+        paypalDispatch({
+          type: "resetOptions",
+          value: {
+            "client-id": paypal.clientId,
+            currency: "USD",
+          },
+        });
+        paypalDispatch({ type: "setLoadingStatus", value: "pending" });
+      };
+      if (order && !order.isPaid) {
+        if (!window.paypal) {
+          loadPaypalScript();
+        }
+      }
+    }
+  }, [errorPayPal, loadingPayPal, order, paypal, paypalDispatch]);
 
-  //  function onApprove(data, actions) {
-  //    return actions.order.capture().then(async function (details) {
-  //      try {
-  //        await payOrder({ orderId, details });
-  //        refetch();
-  //        toast.success('Order is paid');
-  //      } catch (err) {
-  //        toast.error(err?.data?.message || err.error);
-  //      }
-  //    });
-  //  }
+  function onApprove(data, actions) {
+    return actions.order.capture().then(async function (details) {
+      try {
+        await payOrder({ orderId, details });
+        refetch();
+        toast.success("Order is paid");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    });
+  }
 
-  // TESTING ONLY! REMOVE BEFORE PRODUCTION
-  // async function onApproveTest() {
-  //   await payOrder({ orderId, details: { payer: {} } });
-  //   refetch();
+  //   TESTING ONLY! REMOVE BEFORE PRODUCTION
+  async function onApproveTest() {
+    await payOrder({ orderId, details: { payer: {} } });
+    refetch();
 
-  //   toast.success('Order is paid');
-  // }
+    toast.success("Order is paid");
+  }
 
-  // function onError(err) {
-  //   toast.error(err.message);
-  // }
+  function onError(err) {
+    toast.error(err.message);
+  }
 
-  // function createOrder(data, actions) {
-  //   return actions.order
-  //     .create({
-  //       purchase_units: [
-  //         {
-  //           amount: { value: order.totalPrice },
-  //         },
-  //       ],
-  //     })
-  //     .then((orderID) => {
-  //       return orderID;
-  //     });
-  // }
+  function createOrder(data, actions) {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            amount: { value: order.totalPrice },
+          },
+        ],
+      })
+      .then((orderID) => {
+        return orderID;
+      });
+  }
 
   // const deliverHandler = async () => {
   //   await deliverOrder(orderId);
@@ -211,35 +211,34 @@ const OrderScreen = () => {
                   <Col>${order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
-              {/* {!order.isPaid && (
-                  <ListGroup.Item>
-                    {loadingPay && <Loader />}
+              {!order.isPaid && (
+                <ListGroup.Item>
+                  {loadingPay && <Loader />}
 
-                    {isPending ? (
-                      <Loader />
-                    ) : (
-                      <div> */}
-              {/* THIS BUTTON IS FOR TESTING! REMOVE BEFORE PRODUCTION! */}
-              {/* <Button
-                          style={{ marginBottom: '10px' }}
-                          onClick={onApproveTest}
-                        >
-                          Test Pay Order
-                        </Button> */}
+                  {isPending ? (
+                    <Loader />
+                  ) : (
+                    <div>
+                      {/* THIS BUTTON IS FOR TESTING! REMOVE BEFORE PRODUCTION! */}
+                      <Button
+                        style={{ marginBottom: "10px" }}
+                        onClick={onApproveTest}
+                      >
+                        Test Pay Order
+                      </Button>
+                      <div>
+                        <PayPalButtons
+                          createOrder={createOrder}
+                          onApprove={onApprove}
+                          onError={onError}
+                        ></PayPalButtons>
+                      </div>
+                    </div>
+                  )}
+                </ListGroup.Item>
+              )}
 
-              {/* <div>
-                          <PayPalButtons
-                            createOrder={createOrder}
-                            onApprove={onApprove}
-                            onError={onError}
-                          ></PayPalButtons>
-                        </div> */}
-              {/* </div>
-                    )}
-                  </ListGroup.Item>
-                )} */}
-
-              {/* {loadingDeliver && <Loader />} */}
+              {/* {loadingDeliver && <Loader />}  */}
 
               {/* {userInfo &&
                   userInfo.isAdmin &&
@@ -254,7 +253,7 @@ const OrderScreen = () => {
                         Mark As Delivered
                       </Button>
                     </ListGroup.Item>
-                  )} */}
+                  )}  */}
             </ListGroup>
           </Card>
         </Col>
